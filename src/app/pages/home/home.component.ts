c0579b6ae865c41276f81a8a11d26bb8bc0bad5e
nestonzin <nestormacedo95@gmail.com>
2024-02-05 21:00:47 -0300

    configura o provider de HttpClient

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+
+import { CharactersService } from '../../shared/services/characters.service';
+
 import { AutoCompleteModule } from 'primeng/autocomplete';
+
+import { HttpClientModule } from '@angular/common/http';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AutoCompleteModule],
+  imports: [AutoCompleteModule, HttpClientModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  personagens: any;
+
+  ngOnInit() {
+    this.getAllCharacters();
+  }
+
+  constructor(private charactersService: CharactersService) {}
+
+  getAllCharacters() {
+    this.charactersService.getAllCharacters().subscribe((res) => {
+      this.personagens = res;
+      console.log(this.personagens, 'ahsdakjh');
+    });
+  }
+}
