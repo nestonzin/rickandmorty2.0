c24c5980362e5c0750bc886913eb92f97c1aeed1
nestonzin <nestormacedo95@gmail.com>
2024-02-09 12:04:09 -0300

    feat: :sparkles: create home components/ footer

--- src/app/shared/services/characters.service.spec.ts
+++ src/app/shared/services/characters.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
-import { CharactersService } from './CharactersService';
+import { CharactersService } from './characters.service';
 
 describe('CharactersService', () => {
   let service: CharactersService;
