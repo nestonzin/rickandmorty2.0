2d6248cdd43580aa358c6097b2e4e063af3a71ea
nestonzin <nestormacedo95@gmail.com>
2024-02-13 21:31:01 -0300

    feat: :beers: add footer e characterDetail

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,8 +5,20 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  socials = [
+    {
+      name: 'Github',
+      imgUrl: 'https://cdn-icons-png.flaticon.com/512/25/25231.png',
+      url: 'https://github.com/nestonzin',
+    },
+    {
+      name: 'Lindekin',
+      imgUrl:
+        'https://iconape.com/wp-content/files/yd/367773/svg/logo-linkedin-logo-icon-png-svg.png',
+      url: 'https://www.linkedin.com/in/nestormacedojs/',
+    },
+  ];
 }
--- src/app/pages/character-detail/character-detail.component.ts
+++ src/app/pages/character-detail/character-detail.component.ts
@@ -1,19 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { CharactersService } from '../../shared/services/characters.service';
+import { FooterComponent } from '../../components/footer/footer.component';
 @Component({
   selector: 'app-character-detail',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, CommonModule],
   templateUrl: './character-detail.component.html',
   styleUrl: './character-detail.component.scss',
 })
 export class CharacterDetailComponent {
-  id?: number;
+  id: number = 1;
 
-  constructor(private route: ActivatedRoute) {}
+  personagemInfos: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private characterService: CharactersService
+  ) {}
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id'];
+    this.getCharacterInfosById(this.id);
+    //sera que vale mais a pena transformar essa bomba em array ou usar como objeto msm?
+    console.log(typeof this.personagemInfos);
+  }
+
+  getCharacterInfosById(id: number) {
+    this.characterService.getCharacterById(id).subscribe((res) => {
+      this.personagemInfos = res;
+      console.log(this.personagemInfos, 'personagem infos?');
+    });
   }
 }
